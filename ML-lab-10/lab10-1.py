0164265301a46c8ba82e818493a95b364937af0a
harishbiotech <harishbiotechnologist@gmail.com>
2026-03-09 21:16:04 +0530

    ML-lab-10/*,ML-lab-13/*

---
+++ ML-lab-10/lab10-1.py
@@ -0,0 +1,19 @@
+#Implement entropy measure using Python.
+#The function should accept a set of data points and their class labels and return the entropy value.
+import math
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("ENTROPY.csv")
+X = df.iloc[:, :-1]
+y = df.iloc[:, -1]
+def Entropy(y):
+    y_unique=y.value_counts(normalize=True)
+    proportions = y_unique.tolist()
+    entropy=[]
+    s = 0
+    for i in range(len(proportions)):
+        s=s+(-proportions[i])*(math.log(proportions[i],2))
+    entropy.append(s)
+    return entropy
+print(Entropy(y))
---
+++ ML-lab-10/lab10-2.py
@@ -0,0 +1,7 @@
+#Implement information gain measures. The function should accept data points for parents, data points for
+# both children and return an information gain value.
+
+import pandas as pd
+
+df = pd.read_csv('ENTROPY.csv')
+X
