1b7678f1f48f668dc3f94ac628d1409d75fd94cc
harishbiotech <harishbiotechnologist@gmail.com>
2026-03-16 21:08:13 +0530

    ML-lab-8/*

--- ML-lab-8/lab8-3.py
+++ ML-lab-8/lab8-3.py
@@ -1,3 +1,18 @@
 # Implement ordinal encoding and one-hot encoding methods in Python from scratch.
-def ordinal_coding:
-`
+import pandas as pd
+
+class ordinal_encoding():
+    def __init__(self):
+        return
+    def fit_transform(self,data):
+        self.data = data
+        self.categories = {}
+        for col in self.data.columns:
+            for idx, value in enumerate(sorted((self.data[col]).unique())):
+                self.categories[value] = idx
+
+
+
+data=pd.DataFrame({'y':['A','B','C','D'],
+                   'x':['E','F','G','H'],})
+print(ordinal_encoding().fit_transform(data))
