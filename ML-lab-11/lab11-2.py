9346d210ac8e71832bb73db10d52de28990f8245
harishbiotech <harishbiotechnologist@gmail.com>
2026-03-17 19:17:07 +0530

    ML-lab-11/*

--- ML-lab-11/lab11-2.py
+++ ML-lab-11/lab11-2.py
@@ -6,3 +6,4 @@ X = df.drop(columns=['Id', 'Species'])
 y = df['Species'].map({"Iris-setosa": 0, "Iris-versicolor": 1, "Iris-virginica": 2})
 X = X.sort_values(by=['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm'])
 print(X)
+split_points =
