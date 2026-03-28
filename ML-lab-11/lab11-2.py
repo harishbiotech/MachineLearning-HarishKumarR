89e1f8ed3af96d472e11b793988c1c89d0210c08
harishbiotech <harishbiotechnologist@gmail.com>
2026-03-28 16:29:07 +0530

    ML-lab-11/*

--- ML-lab-11/lab11-2.py
+++ ML-lab-11/lab11-2.py
@@ -190,7 +190,7 @@ X, y = load_data()
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Build the tree
-tree = build_tree(X_train, y_train, max_depth=2, min_samples=0)
+tree = build_tree(X_train, y_train, max_depth=5, min_samples=0)
 
 # Predict
 y_pred = predict(tree, X_test)
